e350cdeb152808074e0367cef0da3aaf6506f5b9
liuyikai152 <liuyknet.163.com>
2018-12-14 19:20:10 +0800

    ytq

--- a-takeaway-demo-of-wxapp-master/page/order/order.js
+++ a-takeaway-demo-of-wxapp-master/page/order/order.js
@@ -1,16 +1,17 @@
 Page({
-
   onLoad: function (options) {
     var that = this;
     wx.request({
-      url: 'http://localhost:24380/api/users/GetUsers',
+      url: 'http://localhost:24380/Order/GetOrders',
       method: 'GET',
       success: function (res) {
         console.log(res.data)
         that.setData({
-          sx: res.data,
+          res_data: res.data,
         })
       }
     })
   },
 })
+
+
